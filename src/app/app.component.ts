a99940f432bd559399fbdd849b820796c6227ed3
ArmandoRodGo <armandrodgo@gmail.com>
2019-06-26 13:21:15 +0200

    3 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,49 @@ export class AppComponent {
   title = 'angular02';
   url = 'https://github.com/BCNCodes/mean-stack2/blob/master/presentaciones/Binding.pdf';
   urlarray = ['https://github.com/BCNCodes/mean-stack2/blob/master/presentaciones/Binding.pdf'];
-  valor = true;
+  valorbtn1:Boolean = false;
+  valorbtn2:Boolean = true;
+  valorbtn3:Boolean = true;
+
+  txtBtn1:String = "Habilitado";
+  txtBtn2:String = "deshabilitado";
+  txtBtn3:String = "deshabilitado";
+  /*valorbtnarray = ['false' , 'true'];*/
+
+  textoSaludar ="Este btn esta habilitado";
+  textoActivar ="Activar btn Saludo";
+  textoDesactivar ="Este btn no esta habilitado";
+
+  mostrarAviso(){
+    alert('Has pulsado el botón!');
+  }
+
+
+  activarSaludo() {
+  }
+  desactivarSaludo() {
+  }
+
+  clickbot1(){
+    this.valorbtn1 = true;
+    this.txtBtn1 = "deshabilitado";
+    this.valorbtn2 = false;
+    this.txtBtn2 = "Habilitado";
+    this.valorbtn3 = false;
+    this.txtBtn3 = "Habilitado";
+  }
+
+  clickbot2(){
+    alert('Has pulsado el botón!');
+  }
+
+  clickbot3(){
+    this.valorbtn1 = false;
+    this.txtBtn1 = "Habilitado";
+    this.valorbtn2 = true;
+    this.txtBtn2 = "deshabilitado";
+    this.valorbtn3 = true;
+    this.txtBtn3 = "deshabilitado";
+  }
+
 }
