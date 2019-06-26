fb83207bd2207801c456827da5a503b685a2aa4b
ArmandoRodGo <armandrodgo@gmail.com>
2019-06-26 12:06:40 +0200

    subida angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular02';
+  url = 'https://github.com/BCNCodes/mean-stack2/blob/master/presentaciones/Binding.pdf';
+  urlarray = ['https://github.com/BCNCodes/mean-stack2/blob/master/presentaciones/Binding.pdf'];
+  valor = true;
 }
---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.scss']
+})
+export class MainHeaderComponent implements OnInit {
+
+  //fecha:any;
+  fecha: string;
+
+  constructor() {
+      moment.locale('es');
+      this.fecha = moment().format('LLL');
+  }
+
+  ngOnInit() {
+  }
+}
